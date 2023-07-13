99280716206ec0881b330e71408032da1bc298ad
Casper Engelmann <cen@makeinfluence.com>
2023-07-13 21:16:32 +0200

    Add variant for "htmx-request" in Tailwind CSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,26 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    /**
+     * Tailwind CSS plugin to add variant for "htmx-request"
+     */
+    plugin(function ({ addVariant, e }) {
+      addVariant("htmx-request", ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.${e(
+            `htmx-request${separator}${className}`,
+            className,
+          )}.htmx-request`;
+        });
+      });
+    }),
+  ],
 };
