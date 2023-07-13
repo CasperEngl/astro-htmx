5e3eb91b387a389939a82b2b76d38f105670d8da
Casper Engelmann <cen@makeinfluence.com>
2023-07-13 16:08:21 +0200

    Remove unnecessary import statement for dotenv in env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,5 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
-import "dotenv/config";
 import { loadEnv } from "vite";
 
 const loadedEnv = loadEnv("MODE", process.cwd(), "");
