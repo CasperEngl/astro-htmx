b99c735e74f4d8211c37ea5d16edd48e8262587d
Casper Engelmann <cen@makeinfluence.com>
2023-07-14 15:01:25 +0200

    Refactor Astro config to remove unused React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  integrations: [tailwind(), solidJs(), react()],
-  output: "server"
+  integrations: [tailwind(), solidJs()],
+  output: "server",
 });
